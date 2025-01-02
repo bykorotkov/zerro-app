fbfcd249fe1db0302921b03a5a9ec246ac71fd99
Igor Korotkov <bykorotkov@gmail.com>
2025-01-02 16:26:38 +0400

    posts logic added, added validation pipe, files processing and docker first config

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import * as process from "process";
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {ValidationPipe} from "./pipes/validation.pipe";
 
 async function start() {
     const PORT = process.env.PORT || 5000;
@@ -15,6 +16,8 @@ async function start() {
         .build()
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('/api/docs', app, document)
+
+    app.useGlobalPipes(new ValidationPipe())
 
     await app.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
 }
