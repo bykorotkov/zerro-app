89c005175197143288000ec147bff067dd95bae6
Igor Korotkov <bykorotkov@gmail.com>
2025-01-12 18:22:04 +0400

    All conflict fix

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -9,34 +9,34 @@ import tseslint from 'typescript-eslint'
 // import regexp from 'eslint-plugin-regexp'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended,
-        // 'plugin:react/recommended',
-        // 'plugin:jsx-a11y/recommended',
-        // 'plugin:@typescript-eslint/recommended',
-    ],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    { ignores: ['dist'] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended,
+            // 'plugin:react/recommended',
+            // 'plugin:jsx-a11y/recommended',
+            // 'plugin:@typescript-eslint/recommended',
+        ],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            // 'react': react, // Добавляем плагин React
+            // 'jsx-a11y': jsxA11y, // Добавляем плагин для доступности JSX
+            // '@typescript-eslint': lintPlugin, // Плагин для TypeScript
+            // 'regexp': regexp, // Плагин для регулярных выражений
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": ["error"]
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-        // 'react': react, // Добавляем плагин React
-        // 'jsx-a11y': jsxA11y, // Добавляем плагин для доступности JSX
-        // '@typescript-eslint': lintPlugin, // Плагин для TypeScript
-        // 'regexp': regexp, // Плагин для регулярных выражений
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": ["error"]
-    },
-  },
 )
