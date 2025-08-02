e74361b6a672a7d101cc6c2cbabd9e81bd43ae00
Igor Korotkov <bykorotkov@gmail.com>
2025-08-03 00:27:52 +0400

    another x6 CI fix for github

--- client/src/shared/ui/button/stories/Buttons.stories.tsx
+++ client/src/shared/ui/button/stories/Buttons.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "../ui/Button.tsx";
 
 const meta: Meta<typeof Button> = {
-    title: "ui/button" as const,
+    title: "button" as const,
     component: Button,
 };
 
